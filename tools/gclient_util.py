9cd745491a6484efd0fd8317daacc267463fe004
Yonggang Luo <luoyonggang@gmail.com>
2021-09-03 05:12:51 +0000

    gclient_util.py sync to newest version

--- tools/gclient_util.py
+++ tools/gclient_util.py
@@ -3,14 +3,16 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from __future__ import absolute_import
+from __future__ import print_function
 import os, sys
 
 try:
   # depot_tools may already be in the import path.
   import gclient_utils
-except ImportError, e:
+except ImportError as e:
   # Search the PATH environment variable to find the depot_tools folder.
-  depot_tools = None;
+  depot_tools = None
   paths = os.environ.get('PATH').split(os.pathsep)
   for path in paths:
     if os.path.exists(os.path.join(path, 'gclient_utils.py')):
@@ -18,28 +20,23 @@ except ImportError, e:
       break
 
   if depot_tools is None:
-    print >> sys.stderr, 'Error: could not find depot_tools in PATH.'
+    print('Error: could not find depot_tools in PATH.', file=sys.stderr)
     sys.exit(2)
 
   # Add depot_tools to import path.
   sys.path.append(depot_tools)
   import gclient_utils
 
+
 # Copied from gclient.py python code.
 def RunAction(dir, command):
   """Runs the action."""
-  if command[0] == 'python':
-    # If the hook specified "python" as the first item, the action is a
-    # Python script.  Run it by starting a new copy of the same
-    # interpreter.
-    command[0] = sys.executable
-
   try:
-    gclient_utils.CheckCallAndFilterAndHeader(
-        command, cwd=dir, always=True)
-  except gclient_utils.Error, e:
+    gclient_utils.CheckCallAndFilter(
+        command, cwd=dir, always_show_header=True, print_stdout=True)
+  except gclient_utils.Error as e:
     # Use a discrete exit status code of 2 to indicate that a hook action
     # failed.  Users of this script may wish to treat hook action failures
     # differently from VC failures.
-    print >> sys.stderr, 'Error: %s' % str(e)
+    print('Error: %s' % str(e), file=sys.stderr)
     sys.exit(2)
